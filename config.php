dcd4de4f1d99557a5b0b3ad5ad57493bbc22fd0b
Simon Luis Ambata <simondioresambata@gmail.com>
2025-11-28 10:18:54 +0000

    fixed config

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 $host = "127.0.0.1";   // connect to local MySQL
 $port = 3306;           // default MySQL port internally
 $user = "G9_1";
-$pass = "password1234";
+$pass = "pass1234";
 $db   = "faker";
 ?>
