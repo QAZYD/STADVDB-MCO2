259477b142cfb22b3dbb0012cbc95aa212adf263
Simon Luis Ambata <simondioresambata@gmail.com>
2025-11-30 10:37:42 +0000

    changed script

--- scripts/create_fragment.sh
+++ scripts/create_fragment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # =============================================================================
 # Script: create_fragments.sh
-# Purpose: Create horizontal fragments of the users table for distributed DB
-# Node 1: users 1–50000
-# Node 2: users 50001–100000
+# Purpose: Create horizontal fragments of the Users table for distributed DB
+# Node 1: Users 1–50000
+# Node 2: Users 50001–100000
 # =============================================================================
 
 # MySQL credentials for Node 0
@@ -16,10 +16,10 @@ PORT=3306
 # Output folder (relative to script location)
 OUTPUT_DIR="/var/www/html/myProject/"
 
-# Fragment 1: users 1–50000
-mysqldump -u $USER -p$PASS -h $HOST -P $PORT --where="id BETWEEN 1 AND 50000" $DB users > ${OUTPUT_DIR}faker_users_node1.sql
+# Fragment 1: Users 1–50000
+mysqldump -u $USER -p$PASS -h $HOST -P $PORT --where="id BETWEEN 1 AND 50000" $DB Users > ${OUTPUT_DIR}Users_node1.sql
 
-# Fragment 2: users 50001–100000
-mysqldump -u $USER -p$PASS -h $HOST -P $PORT --where="id BETWEEN 50001 AND 100000" $DB users > ${OUTPUT_DIR}faker_users_node2.sql
+# Fragment 2: Users 50001–100000
+mysqldump -u $USER -p$PASS -h $HOST -P $PORT --where="id BETWEEN 50001 AND 100000" $DB Users > ${OUTPUT_DIR}Users_node2.sql
 
 echo "Fragments created successfully in $OUTPUT_DIR"
