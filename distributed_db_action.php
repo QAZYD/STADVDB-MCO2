13c4ab8cd569c6f04cf6f41845c6be360e1391cb
Simon Luis Ambata <simondioresambata@gmail.com>
2025-12-01 09:01:07 +0000

    distributed

--- distributed_db_action.php
+++ distributed_db_action.php
@@ -12,53 +12,45 @@ $isMaster = ($currentIP === $masterIP);
 
 if (!isset($_POST['run']) || !$isMaster) {
     http_response_code(403);
-    echo "❌ Forbidden: Not master or invalid request.\n";
+    echo "❌ Forbidden: Not master or invalid request.";
     exit;
 }
 
+// Remote servers
 $remoteServers = [
     [ "host" => "10.2.14.130", "user" => "simon" ],
     [ "host" => "10.2.14.131", "user" => "simon" ]
 ];
 
-function runScripts(array $scripts, string $baseDir, array $remoteServers = []) {
-    $log = [];
+// Steps log
+$log = [];
 
-    foreach ($scripts as $index => $script) {
-        $scriptPath = "$baseDir/$script";
-        if (!file_exists($scriptPath)) continue;
-
-        chmod($scriptPath, 0755);
-
-        if ($index === 0) {
-            $cmd = "sudo $scriptPath 2>&1";
-            popen($cmd, 'r');
-            $log[] = "✔ Fragments created";
-        } 
-        elseif ($index === 1) {
-            $cmd = "sudo $scriptPath 2>&1";
-            popen($cmd, 'r');
-            $log[] = "✔ Fragments pushed";
-        } 
-        else {
-            $serverIndex = $index - 2;
-            $server = $remoteServers[$serverIndex];
-            $cmd = "ssh {$server['user']}@{$server['host']} 'bash -s' < $scriptPath 2>&1";
-            popen($cmd, 'r');
-            $log[] = "✔ Fragments server" . ($serverIndex + 1) . " successful";
+function runCommand($cmd, &$log, $message) {
+    $proc = popen($cmd, 'r');
+    if (is_resource($proc)) {
+        while (!feof($proc)) {
+            fgets($proc); // ignore output, we only want simplified log
         }
+        pclose($proc);
     }
-
-    return implode("\n", $log);
+    $log[] = "✔ $message";
 }
-
-$scripts = [
-    "create_fragments.sh",
-    "push_fragments.sh",
-    "import_fragments1.sh",
-    "import_fragments2.sh"
-];
 
 $baseDir = "/var/www/html/myProject/scripts";
 
-echo runScripts($scripts, $baseDir, $remoteServers);
+// 1. Create fragments
+runCommand("sudo $baseDir/create_fragments.sh 2>&1", $log, "Fragments created");
+
+// 2. Push fragments
+runCommand("sudo $baseDir/push_fragments.sh 2>&1", $log, "Fragments pushed");
+
+// 3. Import fragments to server1
+$server = $remoteServers[0];
+runCommand("ssh {$server['user']}@{$server['host']} 'bash -s' < $baseDir/import_fragments1.sh 2>&1", $log, "Fragments server1 successful");
+
+// 4. Import fragments to server2
+$server = $remoteServers[1];
+runCommand("ssh {$server['user']}@{$server['host']} 'bash -s' < $baseDir/import_fragments2.sh 2>&1", $log, "Fragments server2 successful");
+
+// Output with newlines in browser
+echo "<pre>" . implode("\n", $log) . "</pre>";
